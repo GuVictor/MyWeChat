7c3257bc30f49b4c64f0006f06489099e39e3500
GWD <347757054@qq.com>
2017-03-20 09:45:57 +0800

    初始化-我的微信项目

---
+++ MyWeChat/MyWeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWeChat
+//
+//  Created by 古伟东 on 2017/3/20.
+//  Copyright © 2017年 victorgu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyWeChat/MyWeChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyWeChat
+//
+//  Created by 古伟东 on 2017/3/20.
+//  Copyright © 2017年 victorgu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
